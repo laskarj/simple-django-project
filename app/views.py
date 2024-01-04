828580386877222c452948aa311d9c6c0d5add77
laskarj <koks.adekvat@gmail.com>
2024-01-04 19:52:19 +0100

    Create templete wich render list of cars from new view, and set up direction to templates

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
 from app.models import Car
 
-def cars_list(request):
-    return HttpResponse("Car List!")
+def cars_list(request: HttpRequest) -> HttpResponse:
+    car_list = Car.objects.all()
+    context = {"car_list": car_list}
+    return render(
+        request=request,
+        template_name="cars_list.html",
+        context=context
+    )
