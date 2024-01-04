a3fc694339d581b77587de9bbbd744c4e73c89b0
laskarj <koks.adekvat@gmail.com>
2024-01-04 19:23:00 +0100

    Create model Car

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    model = models.CharField(max_length=63)
+    brand = models.CharField(max_length=63)
+    price = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.brand}({self.model})"
